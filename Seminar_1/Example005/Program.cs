9c8d8a1f6c01dcd060f021b1e6f9d2b698239299
Maxim <maxgerasin32@gmail.com>
2022-09-21 01:59:55 +0300

    Seminar_1 - пример 5

---
+++ Seminar_1/Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу, которая на вход принимает одно чило (N),
+// а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);   // Вводит Абсолютное число, чтобы работало и с минусовыми числами
+int num2 = -num;
+while(num2 <= num)
+{
+    Console.Write(num2 + ",");
+    num2 = num2 + 1;
+}
+Console.WriteLine();
