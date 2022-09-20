4b5e57e0c323061c4403947f8a9919916a64be88
Maxim <maxgerasin32@gmail.com>
2022-09-21 01:03:01 +0300

    Seminar_1 - пример 2 и 3

---
+++ Seminar_1/Example002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая на вох принимает 2 числа
+// и проверяет, является ли первое число квадратом второго
+
+Console.WriteLine("Введите число 1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
