a7e4a25debf9559b4b4a87f4d859b877d76d4945
Maxim <maxgerasin32@gmail.com>
2022-09-21 01:31:31 +0300

    Seminar_1 - пример 4

---
+++ Seminar_1/Example004/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления значения функции  возведения числа в квадрат
+// ФУНКЦИЯ !!!!
+
+int Power(int x)
+{
+    return x * x;
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Power(number));
