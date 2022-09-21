dee7b696202eaaa59e3916595e1369be970a94fe
Maxim <maxgerasin32@gmail.com>
2022-09-21 12:55:48 +0300

    Examples008 + 008_2

---
+++ Examples008/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти наибольшее из 9 чиел
+
+int a1 = 12;
+int b1 = 23;
+int a2 = 45;
+int b2 = 44;
+int a3 = 11;
+int b3 = 76;
+int a4 = 75;
+int b4 = 33;
+int c4 = 35;
+
+int max = a1;
+if (b1 > max) max = b1;
+if (a2 > max) max = a2;
+
+if (b2 > max) max = b2;
+if (a3 > max) max = a3;
+
+if (b3 > max) max = b3;
+if (a4 > max) max = a4;
+
+if (b4 > max) max = b4;
+if (c4 > max) max = c4;
+
+Console.WriteLine(max);
+
