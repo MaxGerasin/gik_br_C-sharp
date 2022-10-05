63fc6dcac44c9597c5c9947e27b709cf26db0c04
Maxim <maxgerasin32@gmail.com>
2022-10-05 14:48:34 +0300

    Seminar_5

---
+++ Seminar_5/Example001/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами в промежутке [-9, 9].
+//Найдите сумму отрицательных и положительных элементов массива.
+
+void Zadacha1()
+{
+int size = 12;
+int[] numbers = new int[size];
+FillArray(numbers, -9, 9);
+PrintArray(numbers);
+GetSummNegative(numbers);
+GetSummPositive(numbers);
+Console.WriteLine(GetSummNegative(numbers));
+Console.WriteLine(GetSummPositive(numbers));
+}
+
+int GetSummPositive(int[] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+            sum += numbers[i];
+    }
+    return sum;
+}
+
+int GetSummNegative(int[] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] < 0)
+            sum += numbers[i];
+    }
+    return sum;
+}
+
+void FillArray(int[] numbers,
+                int minValue,
+                int maxValue)
+{
+    maxValue++;
+    Random random = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next(minValue, maxValue);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.WriteLine("Вывод массива");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + "   ");
+    }
+    Console.WriteLine();
+}
+Zadacha1();
---
+++ Seminar_5/Example002/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу замены элементов массива: положительные заменить на соотвествующие отрицательные,
+// и наоборот.
+void Zadacha2()
+{
+    int size = 12;
+    int[] numbers = new int[size];
+    FillArray(numbers, -9, 9);
+    PrintArray(numbers);
+    ChangeValue(numbers);
+    PrintArray(numbers);
+}
+
+Zadacha2();
+
+void ChangeValue(int[] numbers)                // Меняет знаки
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] *= -1;
+    }
+
+}
+
+void FillArray(int[] numbers,
+                int minValue,
+                int maxValue)
+{
+    maxValue++;
+    Random random = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.Next(minValue, maxValue);
+    }
+}
+
+void PrintArray(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+        Console.Write(num[i] + ",   ");
+    }
+    Console.Write("\b\b\b");
+    Console.WriteLine();
+}
